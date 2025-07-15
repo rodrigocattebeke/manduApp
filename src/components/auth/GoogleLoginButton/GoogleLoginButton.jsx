00133132cb1c69b9e62ada5273a8c2fb8b7e005b
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-15 17:50:34 -0300

    feat: create GoogleLoginButton component

--- src/components/auth/GoogleLoginButton/GoogleLoginButton.jsx
+++ src/components/auth/GoogleLoginButton/GoogleLoginButton.jsx
@@ -2,14 +2,12 @@
 import Image from "next/image";
 import styles from "./GoogleLoginButton.module.css";
 
-export const GoogleLoginButton = ({ onClick = null }) => {
-  if (!onClick)
-    onClick = () => {
-      return;
-    };
-
+export const GoogleLoginButton = ({ onLogin = null }) => {
+  const handleLogin = () => {
+    if (onLogin) onLogin();
+  };
   return (
-    <div className={styles.container} onClick={onClick}>
+    <div className={styles.container} onClick={handleLogin}>
       <Image src={"/images/google-logo.png"} alt="Logo de google" width={35} height={35} />
       <p>Continuar con google</p>
     </div>
