9f16a698d121110b72b0b89225a3ddccc5f0f7c4
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-17 16:23:20 -0300

    feat: create StatusCard component

---
+++ src/components/ui/statusCard/StatusCard.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { CARD_STATUS } from "@/constants/cardStatus";
+import styles from "./StatusCard.module.css";
+import Link from "next/link";
+
+export const StatusCard = ({ title = "", quantity = 0, status = undefined, to = "" }) => {
+  if (!status) return console.error(`Se necesita pasar un estado para la card. Estados válidos: ${Object.values(CARD_STATUS).join(", ")}`);
+  if (!Object.values(CARD_STATUS).includes(status)) return console.error(`El valor de status pasado es incorrecto. Estados válidos: ${Object.values(CARD_STATUS).join(", ")}`);
+
+  return (
+    <Link href={to} className={`${styles.card} ${styles[status]}`}>
+      <div className={styles.cardBody}>
+        <h3 className="my-0">{title}</h3>
+        <p className="my-0">{quantity}</p>
+      </div>
+    </Link>
+  );
+};
---
+++ src/constants/cardStatus.js
@@ -0,0 +1,5 @@
+export const CARD_STATUS = {
+  COMPLETE: "complete",
+  PENDING: "pending",
+  ERROR: "error",
+};
