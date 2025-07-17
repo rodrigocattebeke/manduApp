b1efdb26ba1e28e9754e937a68ec6fc1395049a9
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-17 16:57:19 -0300

    feat: create StatusOverview component for homepage

---
+++ src/components/pages/home/statusOverview/StatusOverview.jsx
@@ -0,0 +1,18 @@
+import { StatusCard } from "@/components/ui/statusCard/StatusCard";
+import styles from "./StatusOverview.module.css";
+
+export const StatusOverview = () => {
+  const cards = [
+    { title: "Completados", quantity: 1, status: "complete", to: "" },
+    { title: "Pendientes", quantity: 2, status: "pending", to: "" },
+    { title: "En proceso", quantity: 3, status: "error", to: "" },
+  ];
+
+  return (
+    <div className={`${styles.listsStatusContainer} flex-column`}>
+      {cards.map((card) => (
+        <StatusCard title={card.title} quantity={card.quantity} status={card.status} to={card.to} />
+      ))}
+    </div>
+  );
+};
