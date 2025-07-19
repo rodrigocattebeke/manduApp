397c04285c6d8422deba81816df6c65d53b55f48
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-19 16:43:12 -0300

    feat(icons): add icons for ui components

---
+++ src/components/icons/Add.jsx
@@ -0,0 +1,7 @@
+export const Add = ({ width = "1.5rem", height = "1.5rem" }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={height} viewBox="0 -960 960 960" width={width}>
+      <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+    </svg>
+  );
+};
---
+++ src/components/icons/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = ({ width = "1.5rem", height = "1.5rem" }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={height} viewBox="0 0 24 24" width={width}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </svg>
+  );
+};
