e7e2540abd01d0d7e9269ddfb6c2cafca81b4dd1
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-22 18:14:27 -0300

    feat(page.js): use services functions for get user lists and pass to the components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import { HomePage } from "@/components/pages/home/HomePage";
+import { getFavorites } from "@/services/firestore/getFavorites";
+import { getRecentEdits } from "@/services/firestore/getRecentEdits";
+import { getRecentUpdates } from "@/services/firestore/getRecentUpdates";
+import { getStatusSummary } from "@/services/firestore/getStatusSummary";
 
-export default function Home() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+export default async function Home() {
+  try {
+    const [statusSummary, recentUpdates, favorites, recentEdits] = await Promise.all([getStatusSummary(), getRecentUpdates(), getFavorites(), getRecentEdits()]);
+
+    return (
+      <>
+        <HomePage favorites={favorites} recentEdits={recentEdits} recentUpdates={recentUpdates} statusSummary={statusSummary} />
+      </>
+    );
+  } catch (error) {
+    console.error("error en el home " + error);
+  }
 }
