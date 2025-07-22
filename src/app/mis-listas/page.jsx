701bb4e7dffa0ccf31c75653bcf32565420fc53a
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-22 18:15:30 -0300

    feat(mis-listas): use getUserLists service function and pass to MyLists component

--- src/app/mis-listas/page.jsx
+++ src/app/mis-listas/page.jsx
@@ -1,5 +1,11 @@
 import { MyLists } from "@/components/pages/myLists/MyLists";
+import { getUserLists } from "@/services/firestore/getUserLists";
+import { notFound } from "next/navigation";
 
 export default async function MisListas() {
-  return <MyLists></MyLists>;
+  const userLists = await getUserLists();
+
+  if (!userLists) return notFound();
+
+  return <MyLists lists={userLists}></MyLists>;
 }
