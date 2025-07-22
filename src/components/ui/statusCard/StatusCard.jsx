5d5c5c56701d2c0a21f82e92cb622b40ad35f2a7
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-22 18:12:23 -0300

    refactor(statusCard): get the card href for status_summary_routes

--- src/components/ui/statusCard/StatusCard.jsx
+++ src/components/ui/statusCard/StatusCard.jsx
@@ -1,14 +1,17 @@
 "use client";
-import { STATUS } from "@/constants/statuses";
+import { STATUS, STATUS_SUMMARY_ROUTES } from "@/constants/statuses";
 import styles from "./StatusCard.module.css";
 import Link from "next/link";
 
-export const StatusCard = ({ title = "", quantity = 0, status = undefined, to = "" }) => {
+export const StatusCard = ({ title = "", quantity = 0, status = undefined }) => {
   if (!status) return console.error(`Se necesita pasar un estado para la card. Estados válidos: ${Object.values(STATUS).join(", ")}`);
   if (!Object.values(STATUS).includes(status)) return console.error(`El valor de status pasado es incorrecto. Estados válidos: ${Object.values(STATUS).join(", ")}`);
 
+  //Get the status card route
+  const statusRoute = STATUS_SUMMARY_ROUTES[status] || "#";
+
   return (
-    <Link href={to} className={`${styles.card} ${styles[status]}`}>
+    <Link href={statusRoute} className={`${styles.card} ${styles[status]}`}>
       <div className={styles.cardBody}>
         <h3 className="my-0">{title}</h3>
         <p className="my-0">{quantity}</p>
