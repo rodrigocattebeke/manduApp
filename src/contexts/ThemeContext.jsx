c31d6c09c0aa79b8c16ae256d54976d5b2fdea01
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-28 17:24:32 -0300

    feat: create ThemeContext

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,29 @@
+"use client";
+const { useContext, useState, useEffect } = require("react");
+
+const ThemeContext = useContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme && (theme == "dark" || theme == "light")) return setTheme(theme);
+  });
+
+  const activateDarkTheme = () => {
+    setTheme("dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  };
+
+  const activateLightTheme = () => {
+    setTheme("light");
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  };
+
+  return <ThemeContext.Provider value={{ theme, activateDarkTheme, activateLightTheme }}>{children}</ThemeContext.Provider>;
+};
+
+export { ThemeContext };
