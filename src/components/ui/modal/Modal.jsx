11adacc6b896561198dcd47bafc1e14f0a496638
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-31 17:33:17 -0300

    feat: create modal component

---
+++ src/components/ui/modal/Modal.jsx
@@ -0,0 +1,74 @@
+"use client";
+import { Close } from "@/components/icons/Close";
+import styles from "./Modal.module.css";
+import { Button } from "../button/Button";
+import { useEffect, useState } from "react";
+
+/**
+ * @param {{
+ * title?: "string",
+ * description?: "string",
+ * mode?: "warning" | "success" | "danger" ,
+ * show?: boolean,
+ * onCancel?: ()=>void,
+ * onConfirm: ()=>void,
+ * onClose: ()=>void,
+ * }} params
+ */
+
+export const Modal = ({ title = "Confirmar", description = "", mode = "warning", show = true, onConfirm, onCancel, onClose }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isClose, setIsClose] = useState(false);
+
+  if (mode !== "warning" && mode !== "success" && mode !== "danger") return console.error("Se debe de pasar un valor válido para la prop mode. Valores válidos: danger, success, warning");
+  if (mode !== "success" && (!onConfirm || typeof onConfirm !== "function")) return console.error("Se debe de pasar una funcion onConfirm.");
+  if (!onClose) return console.error("Se necesita pasar una funcion onClose para manejar el cierre/apertura del modal.");
+
+  useEffect(() => {
+    if (show) {
+      setIsMounted(true);
+      setIsClose(false);
+    } else if (!show) {
+      setIsClose(true);
+      const timeout = setTimeout(() => {
+        setIsMounted(false);
+      }, 600);
+      return () => clearTimeout(timeout);
+    }
+  }, [show]);
+
+  //Handle modal close
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    isMounted && (
+      <div className={`${styles.backdrop} ${isClose ? styles.close : styles.show}`}>
+        <div className={styles.modal}>
+          <div className={styles.closeContainer}>
+            <div className={styles.close} onClick={handleClose}>
+              <Close />
+            </div>
+          </div>
+          <div className={styles.modalBody}>
+            <div className={styles.text}>
+              <p className={styles.modalTitle}>{title}</p>
+              <p className={`m-0`}>{description}</p>
+            </div>
+            <div className={styles.actionButtons}>
+              {mode == "success" ? (
+                <Button text="Confirmar" fullWidth="true" mode="success" onClick={onClose} />
+              ) : (
+                <>
+                  <Button text="Cancelar" fullWidth="true" onClick={onCancel} />
+                  <Button text="Confirmar" fullWidth="true" mode={mode == "danger" ? "danger" : "primary"} onClick={onConfirm} />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
