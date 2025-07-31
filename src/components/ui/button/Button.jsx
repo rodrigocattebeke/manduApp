b6547440682e0d4fd187abd6d76de46ff1e9adf6
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-31 15:57:02 -0300

    feat(button): add default style for button

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -3,13 +3,13 @@ import styles from "./Button.module.css";
 /**
  * @param {{
  *  text?: 'string',
- *  mode?: 'primary' | 'secondary' | 'danger',
+ *  mode?:  'default' | 'primary' | 'secondary' | 'danger',
  *  fullWidth?: 'false' | 'true',
  *  onClick?: 'function()',
  * }} props
  */
 
-export const Button = ({ text = "", mode = "primary", fullWidth = false, onClick }) => {
+export const Button = ({ text = "", mode = "default", fullWidth = false, onClick }) => {
   return onClick ? (
     <button className={`${styles.button} ${styles[mode]} ${fullWidth ? styles.fullWidth : ""}`} onClick={onClick}>
       {text}
