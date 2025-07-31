ba3544e29bbc6e28e6296ecdca847c473a756f98
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-07-31 17:34:20 -0300

    feat: create cuenta page component

---
+++ src/components/pages/ajustes/cuenta/Cuenta.jsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Header } from "@/components/ui/header/Header";
+import Image from "next/image";
+import styles from "./Cuenta.module.css";
+import { useContext, useState } from "react";
+import { UserContext } from "@/contexts/UserContext";
+import { Button } from "@/components/ui/button/Button";
+import { Modal } from "@/components/ui/modal/Modal";
+
+export const Cuenta = () => {
+  const { userData } = useContext(UserContext);
+  const [displayName, setDisplayName] = useState(userData.displayName);
+  const [showSaveDataModal, setShowSaveDataModal] = useState(false);
+  const [showDeleteAccountModal, setShowDeleteAccountModal] = useState(false);
+
+  const handleInput = (e) => {
+    setDisplayName(e.target.value);
+  };
+
+  // Save data button functions
+  const onSaveDataModalClose = () => {
+    setShowSaveDataModal(false);
+  };
+
+  const handleSaveDataButton = () => {
+    setShowSaveDataModal(true);
+  };
+
+  const onConfirmSaveData = () => {
+    console.log("Datos actualizados");
+    const timeout = setTimeout(() => {
+      setShowSaveDataModal(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  };
+
+  // Delete account button functions
+  const onDeleteAccountModalClose = () => {
+    setShowDeleteAccountModal(false);
+  };
+
+  const handleDeleteAccountButton = () => {
+    setShowDeleteAccountModal(true);
+  };
+
+  const onConfirmDeleteAccount = () => {
+    console.log("Cuenta eliminada");
+    const timeout = setTimeout(() => {
+      setShowDeleteAccountModal(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  };
+
+  return (
+    <>
+      <Header title="Cuenta" />
+      <section className="container-xxl">
+        <div className={styles.profileImage}>
+          <div className={styles.imageContainer}>
+            <Image src={userData.photoURL} width={128} height={128} alt={`Foto de perfil de ${userData.displayName}`} />
+          </div>
+          <div className={styles.infoContainer}>
+            <h2>{userData.displayName}</h2>
+            <p>Te uniste en 2025</p>
+          </div>
+          <div className={styles.changeImageButton}>
+            <Button text="Cambiar foto de perfil" />
+          </div>
+        </div>
+        <div className={styles.displayName}>
+          <p>Nombre</p>
+          <input name="name" className={styles.nameInput} value={displayName} onChange={handleInput} />
+        </div>
+      </section>
+      <section className={`container-xxl ${styles.actionButtons}`}>
+        <div className={styles.editButtons}>
+          <Button text="Guardar" mode="primary" fullWidth="true" onClick={handleSaveDataButton} />
+          <Button text="Cancelar" fullWidth="true" />
+        </div>
+        <Button text="Eliminar cuenta" mode="danger" fullWidth="true" onClick={handleDeleteAccountButton} />
+      </section>
+
+      {/*    MODALS    */}
+
+      {/* Save data modal*/}
+      <Modal title="¿Estás seguro de guardar los cambios?" show={showSaveDataModal} onClose={onSaveDataModalClose} onConfirm={onConfirmSaveData} onCancel={onSaveDataModalClose} />
+      {/* Delete account modal */}
+      <Modal title="¿Estás seguro de eliminar tu cuenta?" description="Esta acción no se puede revertir" mode="danger" show={showDeleteAccountModal} onClose={onDeleteAccountModalClose} onConfirm={onConfirmDeleteAccount} onCancel={onDeleteAccountModalClose} />
+    </>
+  );
+};
