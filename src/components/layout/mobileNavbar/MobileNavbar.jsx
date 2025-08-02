d4a8a8aed48aec7b32f15afe50c780bdd25dc5ce
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-02 16:11:31 -0300

    feat(MobileNavbar): add Link to the buttons, and add function to detech the actual path

--- src/components/layout/mobileNavbar/MobileNavbar.jsx
+++ src/components/layout/mobileNavbar/MobileNavbar.jsx
@@ -1,36 +1,64 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./MobileNavbar.module.css";
 import { Home } from "@/components/icons/Home";
 import { Folder } from "@/components/icons/Folder";
 import { Search } from "@/components/icons/Search";
 import { Settings } from "@/components/icons/Settings";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const MobileNavbar = ({ customClass = "" }) => {
   const [active, setActive] = useState(0);
+  const pathname = usePathname();
+
+  const buttonsArray = [
+    {
+      icon: <Home />,
+      title: "Inicio",
+      href: "/",
+      id: 1,
+    },
+    {
+      icon: <Search />,
+      title: "Buscar",
+      href: "/buscar",
+      id: 2,
+    },
+    {
+      icon: <Folder />,
+      title: "Mis listas",
+      href: "/mis-listas",
+      id: 3,
+    },
+    {
+      icon: <Settings />,
+      title: "Ajustes",
+      href: "/ajustes",
+      id: 4,
+    },
+  ];
 
   const handleActive = (iconId) => {
     setActive(iconId);
   };
 
+  useEffect(() => {
+    const basePath = pathname.split("/")[1];
+
+    const buttonObject = buttonsArray.find((button) => button.href == `/${basePath}`);
+
+    buttonObject ? setActive(buttonObject.id) : setActive(null);
+  });
+
   return (
     <nav className={`${styles.navbar} ${customClass}`}>
-      <div className={`${styles.icon} ${active == 1 ? styles.active : ""}`} onClick={() => handleActive(1)}>
-        <Home />
-        <small>Inicio</small>
-      </div>
-      <div className={`${styles.icon} ${active == 2 ? styles.active : ""}`} onClick={() => handleActive(2)}>
-        <Search />
-        <small>Buscar</small>
-      </div>
-      <div className={`${styles.icon} ${active == 3 ? styles.active : ""}`} onClick={() => handleActive(3)}>
-        <Folder />
-        <small>Mis Listas</small>
-      </div>
-      <div className={`${styles.icon} ${active == 4 ? styles.active : ""}`} onClick={() => handleActive(4)}>
-        <Settings />
-        <small>Ajustes</small>
-      </div>
+      {buttonsArray.map((button, i) => (
+        <Link href={button.href} className={`${styles.icon} ${active == button.id ? styles.active : ""}`} onClick={() => handleActive(button.id)} key={i}>
+          {button.icon}
+          <small>{button.title}</small>
+        </Link>
+      ))}
     </nav>
   );
 };
