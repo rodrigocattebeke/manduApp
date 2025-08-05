20fc3af08cab6d0a169abe2c1ac3daa2012932a9
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-05 18:16:57 -0300

    feat: create mis-listas/agregar page component

---
+++ src/components/pages/mis-listas/agregar/Agregar.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { FormList } from "@/components/formList/FormList";
+import { Header } from "@/components/ui/header/Header";
+
+export const Agregar = () => {
+  const handleSubmit = (formObject) => {
+    console.log(formObject);
+  };
+
+  return (
+    <>
+      <Header title="Nueva lista" />
+      <section className="container-xxl">
+        <FormList onSubmit={handleSubmit} />
+      </section>
+    </>
+  );
+};
