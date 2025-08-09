fed48a20c34417eeaae22b91cde8a98f5cac9069
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-09 16:31:31 -0300

    feat(HomePage): add Nueva Lista button

--- src/components/pages/inicio/HomePage.jsx
+++ src/components/pages/inicio/HomePage.jsx
@@ -5,19 +5,28 @@ import { UserContext } from "@/contexts/UserContext";
 import { StatusOverview } from "./components/statusOverview/StatusOverview";
 import { ListsSection } from "./components/listsSection/ListsSection";
 import { FloatingAddButton } from "@/components/ui/floatingAddButton/FloatingAddButton";
+import { Button } from "@/components/ui/button/Button";
+import Link from "next/link";
 
 export const HomePage = ({ statusSummary, recentUpdates, favorites, recentEdits }) => {
   const { userData } = useContext(UserContext);
 
   return (
     <>
-      <section className="container-xxl d-flex align-items-center justify-content-center py-3">
+      <section className="container-xxl d-lg-none  d-flex align-items-center justify-content-center py-3">
         <h1 className="my-0">Inicio</h1>
       </section>
-      <section className={`container-xxl`}>
-        <div>
-          <h2 className="my-0">Hola, {userData.displayName.split(" ")[0]}</h2>
-          <p className="my-0">Esto es lo último de tus listas</p>
+      <section className={`container-xxl py-lg-3`}>
+        <div className="d-flex justify-content-between">
+          <div>
+            <h2 className="my-0">Hola, {userData.displayName.split(" ")[0]}</h2>
+            <p className="my-0">Esto es lo último de tus listas</p>
+          </div>
+          <div>
+            <Link href="/mis-listas/agregar">
+              <Button text="+ Nueva Lista" mode="primary" />
+            </Link>
+          </div>
         </div>
         <StatusOverview statusSummary={statusSummary} />
       </section>
