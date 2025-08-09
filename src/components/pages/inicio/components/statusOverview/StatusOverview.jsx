f48598ad7076795334104fc8d53a3bce1db9cdfd
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-09 15:55:52 -0300

    feat(StatusOverview): add flex-md-row to the status container className

--- src/components/pages/inicio/components/statusOverview/StatusOverview.jsx
+++ src/components/pages/inicio/components/statusOverview/StatusOverview.jsx
@@ -5,7 +5,7 @@ export const StatusOverview = ({ statusSummary }) => {
   if (!statusSummary || !Array.isArray(statusSummary)) return console.error("Se debe de pasar el array con los estados.");
 
   return (
-    <div className={`${styles.listsStatusContainer} flex-column`}>
+    <div className={`${styles.listsStatusContainer} flex-column flex-md-row`}>
       {statusSummary.map((card, i) => (
         <StatusCard title={card.title} quantity={card.quantity} status={card.status} key={i} />
       ))}
