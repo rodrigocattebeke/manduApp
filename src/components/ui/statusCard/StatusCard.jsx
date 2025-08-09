8e19878c5919bd987432867413f0a20bd4249d74
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-09 16:19:09 -0300

    feat(StatusCard): remove Link

--- src/components/ui/statusCard/StatusCard.jsx
+++ src/components/ui/statusCard/StatusCard.jsx
@@ -11,11 +11,11 @@ export const StatusCard = ({ title = "", quantity = 0, status = undefined }) => 
   const statusRoute = STATUS_SUMMARY_ROUTES[status] || "#";
 
   return (
-    <Link href={statusRoute} className={`${styles.card} ${styles[status]}`}>
+    <div className={`${styles.card} ${styles[status]}`}>
       <div className={styles.cardBody}>
         <h3 className="my-0">{title}</h3>
         <p className="my-0">{quantity}</p>
       </div>
-    </Link>
+    </div>
   );
 };
