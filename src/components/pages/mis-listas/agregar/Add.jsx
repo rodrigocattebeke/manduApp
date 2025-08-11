29981a6948b50f034969b3d4c6249f2e3476716e
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-11 16:01:03 -0300

    refactor(mis-listas/agregar): adapt header for lg view

--- src/components/pages/mis-listas/agregar/Add.jsx
+++ src/components/pages/mis-listas/agregar/Add.jsx
@@ -10,7 +10,10 @@ export const Add = () => {
 
   return (
     <>
-      <Header title="Nueva lista" />
+      <Header title="Nueva lista" className="d-lg-none" />
+      <header className="d-none d-lg-flex align-items-center justify-content-between p-3">
+        <h1 className="my-0">Nueva lista</h1>
+      </header>
       <section className="container-xxl">
         <FormList onSubmit={handleSubmit} showSelectStatus={false} />
       </section>
