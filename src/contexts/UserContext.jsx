a5800a3a010d4be194c2cd97b6ab84644ad96745
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-15 15:19:02 -0300

    refactor(UserContext): modularize user functions into firestore/services/user/

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,33 +1,13 @@
 "use client";
 
-import { singInWithGoogle } from "@/lib/firebase/auth";
 import { createContext, useEffect, useState } from "react";
-import { getDoc, doc, serverTimestamp, setDoc } from "firebase/firestore";
-import { auth, db } from "@/lib/firebase";
 import { usePathname, useRouter } from "next/navigation";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { updateDisplayNameService } from "@/services/firestore/user/updateDisplayNameService";
+import { singOutService } from "@/services/firestore/user/singOutService";
+import { observeAuthState } from "@/services/firestore/user/observeAuthState";
+import { loginWithGoogleService } from "@/services/firestore/user/loginWithGoogleService";
 
 const UserContext = createContext();
-
-async function createUser(userRef, user) {
-  try {
-    await setDoc(userRef, {
-      uid: user.uid,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      createdAt: serverTimestamp(),
-    });
-
-    return { success: true };
-  } catch (error) {
-    console.error("Error creando usuario:", error);
-    return {
-      success: false,
-      error: error.code || "unknown",
-      message: "No se pudo crear el usuario.",
-    };
-  }
-}
 
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState(undefined);
@@ -37,61 +17,31 @@ export const UserProvider = ({ children }) => {
 
   //Function for login, and if the user doesn't exist, create user in firestore db
   const loginWithGoogle = async () => {
-    try {
-      const user = await singInWithGoogle();
-      setUserData({
-        uid: user.uid,
-        photoURL: user.photoURL,
-        displayName: user.displayName,
-      });
+    const userData = await loginWithGoogleService();
 
-      const userRef = doc(db, "users", user.uid);
-
-      //Search the user in the DB
-      try {
-        //If getDoc returns error, the users doesn't exist in the db
-        const userSnap = await getDoc(userRef);
-
-        //Create the user if user doesn't exist (if don't throw error)
-        if (!userSnap.exists()) {
-          const res = await createUser(userRef, user);
-
-          if (!res.success) console.log("Error al crear usuario"); //Manejo momentaneo, cambiar despues
-        }
-      } catch (error) {
-        if (error.code === "permission-denied") {
-          const res = await createUser(userRef, user);
-
-          if (!res.success) console.log("Error al crear usuario"); //Manejo momentaneo, cambiar despues
-        } else {
-          console.log("Error desconocido, vuelve a intentarlo más tarde");
-        }
-      }
-    } catch (error) {
-      console.log("Ocurrió un error al iniciar sesión, intente más tarde");
+    if (userData.success) {
+      setUserData(userData.user);
+      return { success: true };
+    } else {
+      console.error("Ocurrio un error al iniciar sesion, intentelo más tarde. " + userData.error);
+      return { success: false, error: userData.error };
     }
   };
 
   //Detect if there is no user logged in
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+    const unsubscribe = observeAuthState(({ user }) => {
       if (user) {
-        setUserData({
-          uid: user.uid,
-          photoURL: user.photoURL,
-          displayName: user.displayName,
-        });
-        if (pathname == "/login") router.push("/");
+        setUserData(user);
         setIsLoading(false);
-      } else if (!user && pathname !== "/login") {
-        router.push("/login");
-      } else if (user && pathname == "/login") {
-        router.push("/");
       }
+
+      if (!user && pathname !== "/login") router.push("/login");
+      if (user && pathname == "/login") router.push("/");
     });
 
     return () => unsubscribe();
-  }, [auth, pathname]);
+  }, [pathname]);
 
   // Detect if the current page is /login, and remove loader
 
@@ -102,18 +52,35 @@ export const UserProvider = ({ children }) => {
   }, [pathname]);
 
   // SignOut
-  const logOut = async () => {
-    if (!auth.currentUser) return;
-    try {
-      await signOut(auth);
-    } catch (error) {
-      return console.error("Ocurrió un error al cerrar sesión: " + error);
+  const singOut = async () => {
+    const res = singOutService();
+    if (res.success) {
+      setUserData(null);
     }
+  };
+
+  //functions for update user information
+  const updateUserDisplayName = async (newName) => {
+    const res = await updateDisplayNameService(userData.uid, newName);
+
+    if (res.success) {
+      setUserData({ ...userData, displayName: newName });
+      return { success: true };
+    } else {
+      console.error("Ocurrio un error al cambiar el nombre");
+      return { success: false, error: res.error };
+    }
+  };
+
+  const userFunctions = {
+    updateDisplayName: (name) => {
+      return updateUserDisplayName(name);
+    },
   };
 
   if (isLoading) return null; //for the moment, return null
 
-  return <UserContext.Provider value={{ loginWithGoogle, logOut, userData }}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ loginWithGoogle, singOut, userData, userFunctions }}>{children}</UserContext.Provider>;
 };
 
 export { UserContext };
