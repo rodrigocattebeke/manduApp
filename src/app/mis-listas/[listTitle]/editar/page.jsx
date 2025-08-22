2dfcb6832dfcaf7526e7298a61eaba13d54dc8d4
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 16:42:57 -0300

    feat(listTitle/editar): add getList function and pass to the Edit component

--- src/app/mis-listas/[listTitle]/editar/page.jsx
+++ src/app/mis-listas/[listTitle]/editar/page.jsx
@@ -1,14 +1,41 @@
+"use client";
+
+import { Loader } from "@/components/loader/Loader";
 import { Edit } from "@/components/pages/lista/editar/Edit";
+import { ListsContext } from "@/contexts/ListsContext";
 import { getUserLists } from "@/services/firestore/getUserLists";
-import { notFound } from "next/navigation";
+import { notFound, useParams } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
 
-export default async function EditList() {
-  try {
-    const lists = await getUserLists();
+export default function EditList() {
+  const params = useParams();
+  const [titleSlug, listId] = params.listTitle.split("--id");
+  const { listFunctions } = useContext(ListsContext);
+  const [list, setList] = useState(undefined);
+  const [isSuccess, setIsSuccess] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return <Edit list={lists[0]} />;
-  } catch (error) {
-    console.error(error);
-    return notFound();
-  }
+  useEffect(() => {
+    const getList = async () => {
+      const listRes = await listFunctions.getList(listId);
+      if (listRes.success) {
+        setList(listRes.list);
+        setIsSuccess(true);
+      } else {
+        setIsSuccess(false);
+      }
+
+      setIsLoading(false);
+    };
+    getList();
+
+    return () => {
+      setIsLoading(true);
+      setIsSuccess(undefined);
+      setList(undefined);
+    };
+  }, []);
+
+  if (isLoading) return <Loader />;
+  return <>{!isSuccess ? <h2>Error al obtener los datos, intente mas tarde</h2> : <Edit list={list} />}</>;
 }
