976873aed72ff5f4235030ec2a6eb398815cec9e
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 18:24:05 -0300

    feat(mis-listas/list): add getAllListItems function

--- src/app/mis-listas/[listTitle]/page.jsx
+++ src/app/mis-listas/[listTitle]/page.jsx
@@ -1,20 +1,38 @@
-import { ListView } from "@/components/pages/lista/ListView";
-import { getListItems } from "@/services/firestore/getListItems";
-import { notFound } from "next/navigation";
+"use client";
 
-export default async function ListPage({ params }) {
-  const awaitedParams = await params;
-  const slug = decodeURIComponent(awaitedParams.listTitle);
+import { Loader } from "@/components/loader/Loader";
+import { ListView } from "@/components/pages/lista/ListView";
+import { ListsContext } from "@/contexts/ListsContext";
+import { notFound, useParams } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+
+export default function ListPage() {
+  const params = useParams();
+  const { listsService, itemsService } = useContext(ListsContext);
+  const slug = decodeURIComponent(params.listTitle);
   const [titleSlug, listId] = slug.split("--id");
+  const [listItems, setListItems] = useState(undefined);
+  const [isSuccess, setIsSuccess] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
   if (!listId || !titleSlug) return notFound();
 
-  try {
-    const listItems = await getListItems();
-    return (
-      <>
-        <ListView listTitle={titleSlug} listItems={listItems} />
-      </>
-    );
-  } catch (error) {}
+  useEffect(() => {
+    const getListItems = async () => {
+      const itemsRes = await itemsService.getAllListItems(listId);
+      if (itemsRes.success) {
+        const itemsArray = Object.values(itemsRes.items);
+        setListItems(itemsArray);
+        setIsSuccess(true);
+      } else {
+        setIsSuccess(false);
+      }
+      setIsLoading(false);
+    };
+    getListItems();
+  }, []);
+
+  if (isLoading) return <Loader />;
+
+  return <>{!isSuccess ? <h2>Ocurrio un error al cargar la lista, intente de nuevo más tarde.</h2> : <ListView listTitle={titleSlug} listItems={listItems} />}</>;
 }
