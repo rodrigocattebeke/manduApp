44bea91027ba36c4321b4e196b5dcad21d8f51a2
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 18:21:52 -0300

    feat: create getItemsById util

---
+++ src/utils/getItemsById.js
@@ -0,0 +1,11 @@
+export const getItemsById = (itemsObj, listId = "") => {
+  if (!itemsObj || typeof itemsObj !== "object" || Array.isArray(itemsObj)) return console.error("Se debe de pasar un objeto con los items a filtrar");
+
+  const items = Object.entries(itemsObj).reduce((acc, [id, data]) => {
+    if (data.listId == listId) {
+      acc[id] = data;
+    }
+    return acc;
+  }, {});
+  return items;
+};
