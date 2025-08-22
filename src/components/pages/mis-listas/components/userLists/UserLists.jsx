411cd9de0ada13fd74d10f66be5c97376139e2cc
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 16:33:27 -0300

    refactor(userList): change lists.map for Object.values().map

--- src/components/pages/mis-listas/components/userLists/UserLists.jsx
+++ src/components/pages/mis-listas/components/userLists/UserLists.jsx
@@ -2,12 +2,12 @@ import { ItemCard } from "@/components/ui/itemCard/ItemCard";
 import styles from "./UserLists.module.css";
 
 export const UserLists = ({ lists = undefined }) => {
-  if (!lists || !Array.isArray(lists)) return console.error("Debe de pasar un array de listas. Cada item debe de contener: {title, quantity, img, to}");
+  if (!lists || Array.isArray(lists)) return console.error("Debe de pasar un objeto de listas. Cada prop debe de contener: {title, img, to}");
 
   return (
     <section className={styles.listsContainer} aria-label="Listas del usuario">
-      {lists.map((list, i) => (
-        <ItemCard title={list.title} quantity={list.quantity} imgURL={list.imgURL} to={list.to} key={i} />
+      {Object.values(lists).map((list, i) => (
+        <ItemCard title={list.title} quantity={list.quantity} imgURL={list.imgURL} to={`/mis-listas/${encodeURIComponent(list.title)}--id${list.id}`} key={i} />
       ))}
     </section>
   );
