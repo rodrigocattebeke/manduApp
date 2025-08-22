5fcbf0f356df9fca2f20fa8b0c241609138a11fd
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 16:40:42 -0300

    feat(mis-listas): add fetchAllLists function

--- src/app/mis-listas/page.jsx
+++ src/app/mis-listas/page.jsx
@@ -1,14 +1,31 @@
+"use client";
 import { MyLists } from "@/components/pages/mis-listas/MyLists";
-import { getUserLists } from "@/services/firestore/getUserLists";
-import { notFound } from "next/navigation";
+import { ListsContext } from "@/contexts/ListsContext";
+import { useContext, useEffect, useState } from "react";
 
-export default async function MisListas() {
-  try {
-    const userLists = await getUserLists();
+export default function MisListas() {
+  const { listFunctions, lists } = useContext(ListsContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(undefined);
 
-    return <MyLists lists={userLists}></MyLists>;
-  } catch (error) {
-    console.log(error);
-    return notFound();
-  }
+  useEffect(() => {
+    const fetchAllLists = async () => {
+      const res = await listFunctions.getAllLists();
+      if (res.success) {
+        setIsSuccess(true);
+      } else {
+        setIsSuccess(false);
+      }
+      setIsLoading(false);
+    };
+    fetchAllLists();
+    return () => {
+      setIsLoading(true);
+      setIsSuccess(undefined);
+    };
+  }, []);
+
+  if (isLoading) return <h3>Cargando...</h3>;
+
+  return <>{!isSuccess ? <h2>Error al cargar las listas, intenta mas tarde</h2> : <MyLists lists={lists}></MyLists>}</>;
 }
