f6eb151999c7094e0c2611763041747a5fe27612
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-22 15:22:21 -0300

    feat(ItemCard): remove quantity indicator

--- src/components/ui/itemCard/ItemCard.jsx
+++ src/components/ui/itemCard/ItemCard.jsx
@@ -6,11 +6,11 @@ export const ItemCard = ({ title = "", quantity = 0, imgURL = "", to = undefined
   return (
     <Link className={styles.itemCard} href={to || "#"}>
       <div className={styles.imgContaner}>
-        <img src={imgURL} alt={"foto de " + title} />
+        <Image src={imgURL} alt={"foto de " + title} width={128} height={128} />
       </div>
       <div className={styles.textContainer}>
         <p>{title}</p>
-        <p className={styles.quantity}>{quantity} items</p>
+        {/* <p className={styles.quantity}>{quantity} items</p> */}
       </div>
     </Link>
   );
