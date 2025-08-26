cd35f1b69d68de7eecf348da3fc2e5b2dfae976c
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-26 18:22:16 -0300

    feat(userLists): use toUrlSlug function for transform itemcard url

--- src/components/pages/mis-listas/components/userLists/UserLists.jsx
+++ src/components/pages/mis-listas/components/userLists/UserLists.jsx
@@ -1,5 +1,6 @@
 import { ItemCard } from "@/components/ui/itemCard/ItemCard";
 import styles from "./UserLists.module.css";
+import { toUrlSlug } from "@/utils/toUrlSlug";
 
 export const UserLists = ({ lists = undefined }) => {
   if (!lists || Array.isArray(lists)) return console.error("Debe de pasar un objeto de listas. Cada prop debe de contener: {title, img, to}");
@@ -7,7 +8,7 @@ export const UserLists = ({ lists = undefined }) => {
   return (
     <section className={styles.listsContainer} aria-label="Listas del usuario">
       {Object.values(lists).map((list, i) => (
-        <ItemCard title={list.title} quantity={list.quantity} imgURL={list.imgURL} to={`/mis-listas/${encodeURIComponent(list.title)}--id${list.id}`} key={i} />
+        <ItemCard title={list.title} quantity={list.quantity} imgURL={list.imgURL} to={`/mis-listas/${toUrlSlug(list.title)}--id${list.id}`} key={i} />
       ))}
     </section>
   );
