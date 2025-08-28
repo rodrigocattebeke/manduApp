db5b0d9c9cfec4f17d20b653ebdf34e0cae43b67
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-28 18:26:58 -0300

    feat(formattedFirestoreTimestamp): add a validation if the timestamp is firestoretimestamp or locale date

--- src/utils/formattedFirestoreTimestamp.js
+++ src/utils/formattedFirestoreTimestamp.js
@@ -1,9 +1,15 @@
 import { Timestamp } from "firebase/firestore";
 
-export const formattedFirestoreTimestamp = (firestoreTimestamp) => {
-  const date = firestoreTimestamp.toDate();
+export const formattedFirestoreTimestamp = (timestamp) => {
   const options = { day: "numeric", month: "long", year: "numeric" };
-  const formattedDate = date.toLocaleDateString("es-ES", options);
+  let formattedDate;
+  let date;
+  if (typeof timestamp == "object") {
+    date = timestamp.toDate();
+  } else {
+    date = new Date(timestamp);
+  }
+  formattedDate = date.toLocaleDateString("es-ES", options);
 
   return formattedDate;
 };
