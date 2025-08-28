9f3d2e90af629b3aa895623de9ae3a64d86b801c
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-28 18:29:43 -0300

    feat(itemTitle/Editar): add getItem function

--- src/app/mis-listas/[listTitle]/[itemTitle]/editar/page.jsx
+++ src/app/mis-listas/[listTitle]/[itemTitle]/editar/page.jsx
@@ -1,14 +1,41 @@
+"use client";
+import { Loader } from "@/components/loader/Loader";
 import { Edit } from "@/components/pages/item/editar/Edit";
-import { getListItem } from "@/services/firestore/getListItem";
-import { notFound } from "next/navigation";
+import { ListsContext } from "@/contexts/ListsContext";
+import { usePathname } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
 
-export default async function Editar() {
-  try {
-    const item = await getListItem();
+export default function Editar() {
+  const { itemsService } = useContext(ListsContext);
+  const pathname = usePathname();
+  const sections = pathname.split("/"); //Get the last url section
+  const itemSection = sections[sections.length - 2];
+  const [item, setItem] = useState(undefined);
+  const [isSuccess, setIsSuccess] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return <Edit item={item} />;
-  } catch (error) {
-    console.log(error);
-    return notFound();
-  }
+  const [titleSlug, itemId] = itemSection.split("--id");
+
+  useEffect(() => {
+    const getItem = async () => {
+      const itemRes = await itemsService.getItem(itemId);
+      if (itemRes.success) {
+        setItem(itemRes.item);
+        setIsSuccess(true);
+      } else {
+        setIsSuccess(false);
+      }
+      setIsLoading(false);
+    };
+    getItem();
+
+    return () => {
+      setIsLoading(true);
+      setIsSuccess(undefined);
+      setItem(undefined);
+    };
+  }, []);
+
+  if (isLoading) return <Loader />;
+  return <>{!isSuccess ? <h2>Error al obtener los datos, intente mas tarde</h2> : <Edit item={item} />}</>;
 }
