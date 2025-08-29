3927272e25f656f04a709be83d09bfd12354594f
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-08-29 17:34:54 -0300

    feat(Modal): add loader

--- src/components/ui/modal/Modal.jsx
+++ src/components/ui/modal/Modal.jsx
@@ -3,6 +3,7 @@ import { Close } from "@/components/icons/Close";
 import styles from "./Modal.module.css";
 import { Button } from "../button/Button";
 import { useEffect, useState } from "react";
+import { Loader } from "@/components/loader/Loader";
 
 /**
  * @param {{
@@ -19,6 +20,7 @@ import { useEffect, useState } from "react";
 export const Modal = ({ title = "Confirmar", description = "", mode = "warning", show = true, onConfirm, onCancel, onClose }) => {
   const [isMounted, setIsMounted] = useState(false);
   const [isClose, setIsClose] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   if (mode !== "warning" && mode !== "success" && mode !== "danger") return console.error("Se debe de pasar un valor válido para la prop mode. Valores válidos: danger, success, warning");
   if (mode !== "success" && (!onConfirm || typeof onConfirm !== "function")) return console.error("Se debe de pasar una funcion onConfirm.");
@@ -42,32 +44,43 @@ export const Modal = ({ title = "Confirmar", description = "", mode = "warning",
     onClose();
   };
 
+  // Handle onConfirm
+  const handleOnConfirm = async () => {
+    setIsLoading(true);
+    await onConfirm();
+    setIsLoading(false);
+  };
+
   return (
     isMounted && (
       <div className={`${styles.backdrop} ${isClose ? styles.close : styles.show}`}>
-        <div className={styles.modal}>
-          <div className={styles.closeContainer}>
-            <div className={styles.close} onClick={handleClose}>
-              <Close />
+        {isLoading ? (
+          <Loader fullScreen="true" />
+        ) : (
+          <div className={styles.modal}>
+            <div className={styles.closeContainer}>
+              <div className={styles.close} onClick={handleClose}>
+                <Close />
+              </div>
+            </div>
+            <div className={styles.modalBody}>
+              <div className={styles.text}>
+                <p className={styles.modalTitle}>{title}</p>
+                <p className={`m-0`}>{description}</p>
+              </div>
+              <div className={styles.actionButtons}>
+                {mode == "success" ? (
+                  <Button text="Confirmar" fullWidth="true" mode="success" onClick={onClose} />
+                ) : (
+                  <>
+                    <Button text="Cancelar" fullWidth="true" onClick={onCancel} />
+                    <Button text="Confirmar" fullWidth="true" mode={mode == "danger" ? "danger" : "primary"} onClick={handleOnConfirm} />
+                  </>
+                )}
+              </div>
             </div>
           </div>
-          <div className={styles.modalBody}>
-            <div className={styles.text}>
-              <p className={styles.modalTitle}>{title}</p>
-              <p className={`m-0`}>{description}</p>
-            </div>
-            <div className={styles.actionButtons}>
-              {mode == "success" ? (
-                <Button text="Confirmar" fullWidth="true" mode="success" onClick={onClose} />
-              ) : (
-                <>
-                  <Button text="Cancelar" fullWidth="true" onClick={onCancel} />
-                  <Button text="Confirmar" fullWidth="true" mode={mode == "danger" ? "danger" : "primary"} onClick={onConfirm} />
-                </>
-              )}
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     )
   );
