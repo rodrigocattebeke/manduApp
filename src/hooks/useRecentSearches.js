f9f28e8745d375c8d8f159d901fc491ac4db4327
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-01 15:53:46 -0300

    feat(useRecentSearches): add guard clauses in addSearch function

--- src/hooks/useRecentSearches.js
+++ src/hooks/useRecentSearches.js
@@ -12,7 +12,8 @@ export const useRecentSearches = (uuid) => {
   }, [uuid, key]);
 
   const addSearch = (search) => {
-    if (!uuid || !search) return;
+    if (!uuid) return console.error("Se debe de pasar el uuid");
+    if (!search) return console.error("Se debe de pasar la palabra buscada");
 
     const updated = [search, ...recent.filter((item) => item !== search)].slice(0, 1);
     localStorage.setItem(key, JSON.stringify(updated));
