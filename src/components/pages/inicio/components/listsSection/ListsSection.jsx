a824a61dccf1276efa37211d14fb707d50797d91
Rodrigo Cattebeke <107275449+rodrigocattebeke@users.noreply.github.com>
2025-09-05 18:26:04 -0300

    feat(listsSection): verify if the list array is empty

--- src/components/pages/inicio/components/listsSection/ListsSection.jsx
+++ src/components/pages/inicio/components/listsSection/ListsSection.jsx
@@ -1,23 +1,40 @@
 import { ListCarousel } from "../listCarousel/ListCarousel";
 import styles from "./ListsSection.module.css";
 
-export const ListsSection = ({ favorites, recentEdits, recentUpdates }) => {
-  if (!favorites || !recentEdits || !recentUpdates) console.error("Se necesitan los datos de las listas a mostrar.");
+export const ListsSection = ({ favorites = [], recentEdits = [], recentUpdates = [] }) => {
+  if (!Array.isArray(favorites) || !Array.isArray(recentEdits) || !Array.isArray(recentUpdates)) return console.error("Se debe de pasar un array con las listas.");
 
   return (
     <>
-      <section className={styles.section}>
-        <h2 className="my-0">Listas recientes</h2>
-        <ListCarousel cards={recentUpdates} basePath="mis-listas" />
-      </section>
-      <section className={styles.section}>
-        <h2 className="my-0">Favoritos</h2>
-        <ListCarousel cards={favorites} basePath="mis-listas" />
-      </section>
-      <section className={styles.section}>
-        <h2 className="my-0">Últimos editados</h2>
-        <ListCarousel cards={recentEdits} basePath="mis-listas" />
-      </section>
+      {/* Recent updated */}
+      {recentUpdates.length > 0 ? (
+        <section className={styles.section}>
+          <h2 className="my-0">Listas recientes</h2>
+          <ListCarousel cards={recentUpdates} basePath="mis-listas" />
+        </section>
+      ) : (
+        ""
+      )}
+
+      {/* Favorites lists*/}
+      {favorites.length > 0 ? (
+        <section className={styles.section}>
+          <h2 className="my-0">Favoritos</h2>
+          <ListCarousel cards={favorites} basePath="mis-listas" />
+        </section>
+      ) : (
+        ""
+      )}
+
+      {/* Recently edited */}
+      {recentEdits.length > 0 ? (
+        <section className={styles.section}>
+          <h2 className="my-0">Últimos editados</h2>
+          <ListCarousel cards={recentEdits} basePath="mis-listas" />
+        </section>
+      ) : (
+        ""
+      )}
     </>
   );
 };
