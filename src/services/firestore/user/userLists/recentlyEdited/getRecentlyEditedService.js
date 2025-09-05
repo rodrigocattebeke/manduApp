ed495edcc8048a8eb7b1705df64df7337b92eb90
Rodrigo Cattebeke <107275449+rodrigocattebeke@users.noreply.github.com>
2025-09-05 18:33:34 -0300

    feat: add getRecentlyEditedService

---
+++ src/services/firestore/user/userLists/recentlyEdited/getRecentlyEditedService.js
@@ -0,0 +1,25 @@
+import { auth, db } from "@/lib/firebase";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+
+export const getRecentlyEditedService = async () => {
+  try {
+    const listsRef = collection(db, "lists");
+
+    const q = query(listsRef, where("userUID", "==", auth.currentUser.uid), orderBy("updatedAt", "desc"), limit(6));
+
+    const listsSnap = await getDocs(q);
+
+    if (listsSnap.empty) return { success: true, lists: {} };
+
+    //transform snap array to object
+    const lists = listsSnap.docs.reduce((prev, doc) => {
+      const data = doc.data();
+      prev[data.id] = data;
+      return prev;
+    }, {});
+
+    return { success: true, lists };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
