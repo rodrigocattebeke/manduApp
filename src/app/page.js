f854c69d226557e88559ceee299db1acbd1169c1
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-05 14:38:58 -0300

    feat(app/page): use getFavoritesLists

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,52 @@
+"use client";
+
+import { Loader } from "@/components/loader/Loader";
 import { HomePage } from "@/components/pages/inicio/HomePage";
+import { UserContext } from "@/contexts/UserContext";
 import { getFavorites } from "@/services/firestore/getFavorites";
 import { getRecentEdits } from "@/services/firestore/getRecentEdits";
 import { getRecentUpdates } from "@/services/firestore/getRecentUpdates";
 import { getStatusSummary } from "@/services/firestore/getStatusSummary";
+import { notFound } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
 
-export default async function Home() {
-  try {
-    const [statusSummary, recentUpdates, favorites, recentEdits] = await Promise.all([getStatusSummary(), getRecentUpdates(), getFavorites(), getRecentEdits()]);
+export default function Home() {
+  const { userFunctions } = useContext(UserContext);
+  const [statusSummaryArray, setStatusSummaryArray] = useState([]);
+  const [recentUpdatedArray, setRecentUpdatedArray] = useState([]);
+  const [favoritesListsArray, setFavoritesListsArray] = useState([]);
+  const [recentlyEditedArray, setRecentlyEditedArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-    return (
-      <>
-        <HomePage favorites={favorites} recentEdits={recentEdits} recentUpdates={recentUpdates} statusSummary={statusSummary} />
-      </>
-    );
-  } catch (error) {
-    console.error("error en el home " + error);
-  }
+  useEffect(() => {
+    const getAllLists = async () => {
+      const results = await Promise.allSettled([getStatusSummary(), getRecentUpdates(), userFunctions.getFavoritesLists(), getRecentEdits()]);
+
+      //Filter fullfilled responses
+      const fulfilled = results.filter((res) => res.status == "fulfilled");
+
+      if (fulfilled.length == 0) return setIsError(true);
+
+      const [statusSummaryRes, recentUpdatedRes, favoritesListsRes, recentlyEditedRes] = results;
+
+      setStatusSummaryArray(statusSummaryRes.value);
+      setRecentUpdatedArray(recentUpdatedRes.value);
+      if (favoritesListsRes.value.success) {
+        setFavoritesListsArray(Object.values(favoritesListsRes.value.lists));
+      }
+      setRecentlyEditedArray(recentlyEditedRes.value);
+      setIsLoading(false);
+    };
+
+    getAllLists();
+  }, [userFunctions]);
+
+  if (isLoading) return <Loader />;
+  if (isError) return notFound();
+  return (
+    <>
+      <HomePage favorites={favoritesListsArray} recentEdits={recentlyEditedArray} recentUpdates={recentUpdatedArray} statusSummaryArray={statusSummaryArray} />
+    </>
+  );
 }
