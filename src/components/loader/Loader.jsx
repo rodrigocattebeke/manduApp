71dae9ce0c9e28c54601dfd59517772ea547ece3
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-08 17:34:06 -0300

    refactor(Loader): change the loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,3 +1,8 @@
-export const Loader = ({ fullScreen = false, fullWidth = false }) => {
-  return <div>Cargando...</div>;
+import styles from "./Loader.module.css";
+export const Loader = ({ fullScreen = false, fullWidth = false, backdrop = false }) => {
+  return (
+    <div className={`${styles.loaderContainer} ${fullScreen ? styles.fullScreen : ""} ${fullWidth ? styles.fullWidth : ""} ${backdrop ? styles.backdrop : ""}`}>
+      <span className={`${styles.loader}`}></span>
+    </div>
+  );
 };
