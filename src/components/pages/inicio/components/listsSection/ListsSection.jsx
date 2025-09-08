a289608f8387e5522ebd2b8cac160a3227dc7dd7
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-08 16:00:24 -0300

    refactor(ListsSection): rename recentEdited prop to recentUpdated

--- src/components/pages/inicio/components/listsSection/ListsSection.jsx
+++ src/components/pages/inicio/components/listsSection/ListsSection.jsx
@@ -1,16 +1,16 @@
 import { ListCarousel } from "../listCarousel/ListCarousel";
 import styles from "./ListsSection.module.css";
 
-export const ListsSection = ({ favorites = [], recentEdits = [], recentUpdates = [] }) => {
-  if (!Array.isArray(favorites) || !Array.isArray(recentEdits) || !Array.isArray(recentUpdates)) return console.error("Se debe de pasar un array con las listas.");
+export const ListsSection = ({ favorites = [], recentCreated = [], recentUpdated = [] }) => {
+  if (!Array.isArray(favorites) || !Array.isArray(recentCreated) || !Array.isArray(recentUpdated)) return console.error("Se debe de pasar un array con las listas.");
 
   return (
     <>
       {/* Recent updated */}
-      {recentUpdates.length > 0 ? (
+      {recentCreated.length > 0 ? (
         <section className={styles.section}>
           <h2 className="my-0">Listas recientes</h2>
-          <ListCarousel cards={recentUpdates} basePath="mis-listas" />
+          <ListCarousel cards={recentCreated} basePath="mis-listas" />
         </section>
       ) : (
         ""
@@ -27,10 +27,10 @@ export const ListsSection = ({ favorites = [], recentEdits = [], recentUpdates =
       )}
 
       {/* Recently edited */}
-      {recentEdits.length > 0 ? (
+      {recentUpdated.length > 0 ? (
         <section className={styles.section}>
           <h2 className="my-0">Últimos editados</h2>
-          <ListCarousel cards={recentEdits} basePath="mis-listas" />
+          <ListCarousel cards={recentUpdated} basePath="mis-listas" />
         </section>
       ) : (
         ""
