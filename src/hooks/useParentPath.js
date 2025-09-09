959c6f40054a92a4151d6abf2f1dcdb880663979
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-09 17:02:50 -0300

    feat(hooks): add useParentPath

---
+++ src/hooks/useParentPath.js
@@ -0,0 +1,8 @@
+import { usePathname } from "next/navigation";
+
+export const useParentPath = () => {
+  const currentPath = usePathname();
+  const parts = currentPath.split("/");
+  parts.pop();
+  return parts.join("/");
+};
