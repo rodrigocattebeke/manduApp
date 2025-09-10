78ee939d290da7b955c7ec180802ba4dbc3d3df2
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-10 15:52:17 -0300

    refactor(ui/Header): add useParentPath to return to back

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -1,11 +1,21 @@
+"use client";
 import { ArrowBack } from "@/components/icons/ArrowBack";
 import styles from "./Header.module.css";
+import { useParentPath } from "@/hooks/useParentPath";
+import { useRouter } from "next/navigation";
 
 export const Header = ({ title = "", className = "" }) => {
+  const parentPath = useParentPath();
+  const router = useRouter();
+
+  const handleArrowClick = () => {
+    router.push(parentPath);
+  };
+
   return (
     <header className={`${className} container-xxl d-flex py-3`}>
       <div className={styles.arrowBackContainer}>
-        <div className={styles.arrowBack}>
+        <div className={styles.arrowBack} onClick={handleArrowClick}>
           <ArrowBack />
         </div>
       </div>
