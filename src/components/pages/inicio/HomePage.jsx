e3fc1ffb03509ca4079becd0c71be3743232fbe7
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-12 16:54:34 -0300

    refactor(HomePage): show emptyState when there are no lists

--- src/components/pages/inicio/HomePage.jsx
+++ src/components/pages/inicio/HomePage.jsx
@@ -7,32 +7,39 @@ import { ListsSection } from "./components/listsSection/ListsSection";
 import { FloatingAddButton } from "@/components/ui/floatingAddButton/FloatingAddButton";
 import { Button } from "@/components/ui/button/Button";
 import Link from "next/link";
+import { EmptyState } from "@/components/ui/emptyState/EmptyState";
+import { AddFiles } from "@/components/icons/AddFiles";
 
-export const HomePage = ({ statusSummary, recentCreated, favorites, recentUpdated }) => {
+export const HomePage = ({ statusSummary, recentCreated = [], favorites = [], recentUpdated = [] }) => {
   const { userData } = useContext(UserContext);
-
   return (
     <>
       <header className="container-xxl d-lg-none  d-flex align-items-center justify-content-center py-3">
         <h1 className="my-0">Inicio</h1>
       </header>
-      <section className={`container-xxl py-lg-3`}>
-        <div className="d-flex justify-content-between">
-          <div>
-            <h2 className="my-0">Hola, {userData.displayName.split(" ")[0]}</h2>
-            <p className="my-0">Esto es lo último de tus listas</p>
-          </div>
-          <div className="d-none d-lg-flex align-items-center">
-            <Link href="/mis-listas/agregar">
-              <Button text="+ Nueva Lista" mode="primary" />
-            </Link>
-          </div>
-        </div>
-        <StatusOverview statusSummary={statusSummary} />
-      </section>
-      <section className="container-xxl my-4 " aria-label="Resumen de listas del usuario">
-        <ListsSection favorites={favorites} recentCreated={recentCreated} recentUpdated={recentUpdated} />
-      </section>
+      {recentCreated.length == 0 && recentUpdated.length == 0 ? (
+        <EmptyState icon={AddFiles} message={"Empieza organizando tus ideas"} action={<Link href={"/mis-listas/agregar"}>¡Agrega tu primera lista!</Link>} />
+      ) : (
+        <>
+          <section className={`container-xxl py-lg-3`}>
+            <div className="d-flex justify-content-between">
+              <div>
+                <h2 className="my-0">Hola, {userData.displayName.split(" ")[0]}</h2>
+                <p className="my-0">Esto es lo último de tus listas</p>
+              </div>
+              <div className="d-none d-lg-flex align-items-center">
+                <Link href="/mis-listas/agregar">
+                  <Button text="+ Nueva Lista" mode="primary" />
+                </Link>
+              </div>
+            </div>
+            <StatusOverview statusSummary={statusSummary} />
+          </section>
+          <section className="container-xxl my-4 " aria-label="Resumen de listas del usuario">
+            <ListsSection favorites={favorites} recentCreated={recentCreated} recentUpdated={recentUpdated} />
+          </section>
+        </>
+      )}
       <FloatingAddButton to={"/mis-listas/agregar"} className="d-lg-none" />
     </>
   );
