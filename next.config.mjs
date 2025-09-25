e8580aaa98cc89a7d1dfe55ec2755aaef2419b9b
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-25 17:38:32 -0300

    chore(next.config): add cloudinary url to the image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["picsum.photos", "lh3.googleusercontent.com"],
+    domains: ["picsum.photos", "lh3.googleusercontent.com", "res.cloudinary.com"],
   },
 };
 
