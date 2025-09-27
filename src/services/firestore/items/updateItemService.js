c3e8e18a6acfb4e53ab10835cfbe7f2210d7e55b
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-27 17:02:43 -0300

    refactor(updateItemService): handle imgFile

--- src/services/firestore/items/updateItemService.js
+++ src/services/firestore/items/updateItemService.js
@@ -1,17 +1,43 @@
 import { db } from "@/lib/firebase";
 import { doc, serverTimestamp, Timestamp, updateDoc } from "firebase/firestore";
+import { uploadItemImageService } from "./uploadItemImageService";
 
 export const updateItemService = async (itemId, newItemData) => {
   try {
+    //Initialize itemRef
     const itemRef = doc(db, "items", itemId);
-
     const localTimestamp = Timestamp.fromDate(new Date());
 
-    const newDataWithLocaleUpdatedAt = { ...newItemData, updatedAt: localTimestamp };
+    if (!newItemData.imgFile) {
+      //Delete imgFile
+      delete newItemData.imgFile;
 
-    await updateDoc(itemRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+      const newDataWithLocaleUpdatedAt = { ...newItemData, updatedAt: localTimestamp };
 
-    return { success: true, item: newDataWithLocaleUpdatedAt };
+      await updateDoc(itemRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+
+      return { success: true, item: newDataWithLocaleUpdatedAt };
+    } else {
+      const imgFile = newItemData.imgFile;
+      // Get new imgURL
+      const res = await uploadItemImageService(imgFile, itemId);
+
+      if (res.success) {
+        //Delete imgFile
+        delete newItemData.imgFile;
+
+        //Set the new imgURL
+        const imgURL = res.imgURL;
+
+        const newDataWithLocaleUpdatedAt = { ...newItemData, imgURL, updatedAt: localTimestamp };
+
+        await updateDoc(itemRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+
+        return { success: true, item: newDataWithLocaleUpdatedAt };
+      } else {
+        return { success: false, error: res.error };
+      }
+    }
   } catch (error) {
     return { success: false, error: error.message || String(error) };
   }
