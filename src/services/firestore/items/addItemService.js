b9f3a20e4c47b6c71f8b91a58ab0136acfde5f31
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-27 15:58:27 -0300

    refactor(firestore/items): handle imgFile in addItemService

--- src/services/firestore/items/addItemService.js
+++ src/services/firestore/items/addItemService.js
@@ -1,19 +1,47 @@
 import { auth, db } from "@/lib/firebase";
 import { collection, doc, serverTimestamp, setDoc, Timestamp } from "firebase/firestore";
+import { uploadItemImageService } from "./uploadItemImageService";
 
 export const addItemService = async (listId, itemData) => {
   try {
+    // Initialize itemRef
     const itemRef = doc(collection(db, "items"));
-
     const localTimestamp = Timestamp.fromDate(new Date());
 
-    const imgURL = "https://picsum.photos/200"; //Set default img url, change in the future with cloudinary api for save photos
+    if (!itemData.imgFile) {
+      //Set default img url
+      const imgURL = "https://picsum.photos/200";
 
-    const dataWithUID = { ...itemData, id: itemRef.id, listId, userUID: auth.currentUser.uid, createdAt: localTimestamp, updatedAt: localTimestamp, imgURL };
+      // Delete imgFile prop
+      delete itemData.imgFile;
 
-    await setDoc(itemRef, { ...dataWithUID, createdAt: serverTimestamp(), updatedAt: serverTimestamp() });
+      const dataWithUID = { ...itemData, id: itemRef.id, listId, userUID: auth.currentUser.uid, createdAt: localTimestamp, updatedAt: localTimestamp, imgURL };
 
-    return { success: true, item: dataWithUID };
+      await setDoc(itemRef, { ...dataWithUID, createdAt: serverTimestamp(), updatedAt: serverTimestamp() });
+
+      return { success: true, item: dataWithUID };
+    } else {
+      const itemId = itemRef.id;
+      const imgFile = itemData.imgFile;
+
+      const res = await uploadItemImageService(imgFile, itemId);
+
+      if (res.success) {
+        // Delete imgFile prop
+        delete itemData.imgFile;
+
+        // Get imgURL
+        const imgURL = res.imgURL;
+
+        const dataWithUID = { ...itemData, id: itemRef.id, listId, userUID: auth.currentUser.uid, createdAt: localTimestamp, updatedAt: localTimestamp, imgURL };
+
+        await setDoc(itemRef, { ...dataWithUID, createdAt: serverTimestamp(), updatedAt: serverTimestamp() });
+
+        return { success: true, item: dataWithUID };
+      } else {
+        return { success: false, error: res.error };
+      }
+    }
   } catch (error) {
     return { success: false, error: error.message || String(error) };
   }
