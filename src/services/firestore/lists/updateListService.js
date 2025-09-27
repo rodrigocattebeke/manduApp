892f36256766e28d992045f6ca8197853c6d61d8
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-09-27 15:36:51 -0300

    refactor(updateListService): handle imgFile

--- src/services/firestore/lists/updateListService.js
+++ src/services/firestore/lists/updateListService.js
@@ -1,17 +1,48 @@
 import { db } from "@/lib/firebase";
 import { doc, updateDoc, serverTimestamp, Timestamp } from "firebase/firestore";
+import { uploadListImageService } from "./uploadListImageService";
 
 export async function updateListService(listID, newListData) {
   try {
-    const listRef = doc(db, "lists", listID);
+    if (!newListData.imgFile) {
+      //delete imgFile of newListData
+      delete newListData.imgFile;
 
-    const localTimestamp = Timestamp.fromDate(new Date());
+      const listRef = doc(db, "lists", listID);
 
-    const newDataWithLocaleUpdatedAt = { ...newListData, updatedAt: localTimestamp };
+      const localTimestamp = Timestamp.fromDate(new Date());
 
-    await updateDoc(listRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+      const newDataWithLocaleUpdatedAt = { ...newListData, updatedAt: localTimestamp };
 
-    return { success: true, list: newDataWithLocaleUpdatedAt };
+      await updateDoc(listRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+
+      return { success: true, list: newDataWithLocaleUpdatedAt };
+    } else {
+      // Upload img and get imgURL
+      const res = await uploadListImageService(newListData.imgFile, listID);
+
+      if (res.success) {
+        //delete imgFile prop of newListData
+        delete newListData.imgFile;
+
+        // get imgURL and set to newListData
+        const imgURL = res.imgURL;
+
+        newListData.imgURL = imgURL;
+
+        const listRef = doc(db, "lists", listID);
+
+        const localTimestamp = Timestamp.fromDate(new Date());
+
+        const newDataWithLocaleUpdatedAt = { ...newListData, updatedAt: localTimestamp };
+
+        await updateDoc(listRef, { ...newDataWithLocaleUpdatedAt, updatedAt: serverTimestamp() });
+
+        return { success: true, list: newDataWithLocaleUpdatedAt };
+      } else {
+        return { success: false, error: res.error };
+      }
+    }
   } catch (error) {
     return { success: false, error: error.message || String(error) };
   }
