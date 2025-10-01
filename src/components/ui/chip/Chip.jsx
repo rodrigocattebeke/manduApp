731d85ce824b8a198c076ae9a8693600a8313587
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-01 17:35:56 -0300

    refactor(Chip): destructuring and add className prop to chip

--- src/components/ui/chip/Chip.jsx
+++ src/components/ui/chip/Chip.jsx
@@ -1,10 +1,20 @@
 import { Close } from "@/components/icons/Close";
 import styles from "./Chip.module.css";
 
-export const Chip = ({ text = "", showClose = false, onClose, ...props }) => {
+export const Chip = ({ text = "", icon: Icon, showClose = false, onClose, className, ...props }) => {
   if (showClose && (!onClose || typeof onclose !== "function")) return console.error("Para manejar el closeButton, se necesita pasar una function onClose");
-  return (
-    <div className={styles.chip} title={text} {...props}>
+  return !Icon ? (
+    <div className={`${styles.chip} ${className}`} title={text} {...props}>
+      <p>{text}</p>
+      <div className={`${styles.close} ${showClose ? styles.show : ""}`}>
+        <div onClick={onClose}>
+          <Close width="1.2rem" height="1.2rem" />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className={`${styles.chip} ${className}`} title={text} {...props}>
+      <Icon />
       <p>{text}</p>
       <div className={`${styles.close} ${showClose ? styles.show : ""}`}>
         <div onClick={onClose}>
