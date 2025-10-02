9f679acb091d5cdfceacf376cba6115671d569aa
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-02 17:47:55 -0300

    refactor(Search): handle searchResults

--- src/components/pages/buscar/Search.jsx
+++ src/components/pages/buscar/Search.jsx
@@ -11,12 +11,15 @@ import { UserContext } from "@/contexts/UserContext";
 import { searchService } from "@/services/firestore/search/searchService";
 import { Loader } from "@/components/loader/Loader";
 import { toUrlSlug } from "@/utils/toUrlSlug";
+import { EmptyState } from "@/components/ui/emptyState/EmptyState";
+import { SearchFiles } from "@/components/icons/SearchFiles";
+import { Empty } from "@/components/icons/Empty";
 
 export const Search = () => {
   const { userData } = useContext(UserContext);
   const { addSearch, recent } = useRecentSearches(userData.userUID);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchResults, setSearchResults] = useState(null);
+  const [searchResults, setSearchResults] = useState(undefined);
   const [query, setQuery] = useState(undefined);
   const queryTimeoutRef = useRef();
 
@@ -45,12 +48,12 @@ export const Search = () => {
 
       if (res.success) {
         if (Object.keys(res.results.lists).length == 0 && Object.keys(res.results.items).length == 0) {
-          setSearchResults(undefined);
+          setSearchResults({});
         } else {
           setSearchResults(res.results);
         }
       } else {
-        setSearchResults(undefined);
+        setSearchResults({});
       }
 
       setIsLoading(false);
@@ -78,22 +81,21 @@ export const Search = () => {
       )}
       {isLoading ? (
         <Loader fullWidth="true" />
+      ) : !searchResults ? (
+        //Show empty state if searchResults is falsy
+        <EmptyState icon={SearchFiles} message={"Busca entre tus listas e items de forma rápida"} className={"mt-2"} />
+      ) : Object.keys(searchResults).length == 0 ? ( //Handle searchResults
+        <EmptyState icon={Empty} message={"No se encontraron resultados para la búsqueda"} className={"mt-2"} />
       ) : (
         <section className={`${styles.resultsContainer} container-xxl`}>
-          {searchResults == null ? (
-            ""
-          ) : searchResults ? (
-            <>
-              {Object.values(searchResults.lists).map((result, i) => (
-                <ItemCard title={result.title} imgURL={result.imgURL} type={"Lista"} to={`/mis-listas/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
-              ))}
-              {Object.values(searchResults.items).map((result, i) => (
-                <ItemCard title={result.title} imgURL={result.imgURL} type={"Item"} to={`/mis-listas/_--id${result.listId}/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
-              ))}
-            </>
-          ) : (
-            <p>No hay resultados de busqueda</p>
-          )}
+          <>
+            {Object.values(searchResults.lists).map((result, i) => (
+              <ItemCard title={result.title} imgURL={result.imgURL} type={"Lista"} to={`/mis-listas/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
+            ))}
+            {Object.values(searchResults.items).map((result, i) => (
+              <ItemCard title={result.title} imgURL={result.imgURL} type={"Item"} to={`/mis-listas/_--id${result.listId}/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
+            ))}
+          </>
         </section>
       )}
     </>
