cb48e8c2f00f23c3c910a6b78574951fad585819
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-02 16:42:32 -0300

    refactor(EmptyState): add className to passed props

--- src/components/ui/emptyState/EmptyState.jsx
+++ src/components/ui/emptyState/EmptyState.jsx
@@ -1,8 +1,8 @@
 import styles from "./EmptyState.module.css";
 
-export const EmptyState = ({ icon: Icon, message, action }) => {
+export const EmptyState = ({ icon: Icon, message, action, className }) => {
   return (
-    <div className={`${styles.emptyState}`}>
+    <div className={`${styles.emptyState} ${className}`}>
       {Icon && <Icon width="10rem" height="10rem" />}
       <p className={`${styles.message}`}>{message}</p>
       {action && <div className={`${styles.action}`}>{action}</div>}
