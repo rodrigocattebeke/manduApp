224ffe76b44c62644039a6a4392c4a7f66120327
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-07 17:56:37 -0300

    refactor(ItemCard): add status prop

--- src/components/ui/itemCard/ItemCard.jsx
+++ src/components/ui/itemCard/ItemCard.jsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 import styles from "./ItemCard.module.css";
 import Link from "next/link";
 
-export const ItemCard = ({ title = "", quantity = 0, type, imgURL = "", to = undefined }) => {
+export const ItemCard = ({ title = "", quantity = 0, imgURL = "", to = undefined, type, status = "" }) => {
   return (
     <Link className={styles.itemCard} href={to || "#"}>
       {type ? (
-        <div className={styles.typeContainer}>
-          <p className={styles.typeText}>{type}</p>
+        <div className={`${styles.typeContainer}`}>
+          <p className={`${styles.typeText}`}>{type}</p>
         </div>
       ) : (
         ""
       )}
+      {status ? <span className={`${styles.status} ${styles[status]}`}></span> : ""}
+
       <div className={styles.imgContaner}>
         <Image src={imgURL} alt={"foto de " + title} width={128} height={128} />
       </div>
