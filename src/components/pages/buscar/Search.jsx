51d51980e04644033b5670a36c4967022e6614a8
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-08 16:13:33 -0300

    refactor(Search page): handle search with search param passed from home

--- src/components/pages/buscar/Search.jsx
+++ src/components/pages/buscar/Search.jsx
@@ -14,16 +14,68 @@ import { toUrlSlug } from "@/utils/toUrlSlug";
 import { EmptyState } from "@/components/ui/emptyState/EmptyState";
 import { SearchFiles } from "@/components/icons/SearchFiles";
 import { Empty } from "@/components/icons/Empty";
+import { SEARCH_FILTER_OPTIONS } from "@/constants/statuses";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export const Search = () => {
+  //    User
   const { userData } = useContext(UserContext);
   const { addSearch, recent } = useRecentSearches(userData.userUID);
-  const [isLoading, setIsLoading] = useState(false);
+
+  //    Router
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  //    Search
   const [searchResults, setSearchResults] = useState(undefined);
-  const [query, setQuery] = useState(undefined);
-  const [filterSelected, setFilterSelected] = useState(undefined);
+  const [query, setQuery] = useState("");
+  const [filterParam, setFilterParam] = useState(undefined);
+  const searchParam = searchParams.get("filter");
+  const [filterSelected, setFilterSelected] = useState(searchParam || SEARCH_FILTER_OPTIONS.all.value);
+
+  //    Refs
   const queryTimeoutRef = useRef();
+
+  //    UI states
+  const [hasInteracted, setHasInteracted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchParam) {
+      setFilterParam(searchParam);
+      router.replace(pathname);
+    }
+  }, []);
+
+  // search function
+  const getSearch = async () => {
+    setIsLoading(true);
+    const res = await searchService(query, filterSelected);
+    if (res.success) {
+      if (Object.keys(res.results.lists).length == 0 && Object.keys(res.results.items).length == 0) {
+        setSearchResults({});
+      } else {
+        setSearchResults(res.results);
+      }
+    } else {
+      setSearchResults({});
+    }
+
+    setIsLoading(false);
+
+    //update recent searches
+    if (hasInteracted) {
+      addSearch(query);
+    }
+  };
+
+  // First search if filter is passed in search params
+  useEffect(() => {
+    if (!hasInteracted && filterParam) {
+      getSearch();
+    }
+  }, [hasInteracted, filterSelected, filterParam]);
 
   //    Handle filters
   const onFilterChange = (filter) => {
@@ -40,6 +92,10 @@ export const Search = () => {
       clearTimeout(queryTimeoutRef.current);
     }
     const timeout = setTimeout(async () => {
+      if (query.trim()) {
+        setHasInteracted(true);
+      }
+
       setQuery(query);
     }, 1500);
 
@@ -48,35 +104,15 @@ export const Search = () => {
 
   //search when the query changes
   useEffect(() => {
-    const getSearch = async () => {
-      if (!query) return;
-      setIsLoading(true);
-      const res = await searchService(query);
-
-      if (res.success) {
-        if (Object.keys(res.results.lists).length == 0 && Object.keys(res.results.items).length == 0) {
-          setSearchResults({});
-        } else {
-          setSearchResults(res.results);
-        }
-      } else {
-        setSearchResults({});
-      }
-
-      setIsLoading(false);
-
-      //update recent searches
-      addSearch(query);
-    };
-
+    if (!query || !query.trim()) return;
     getSearch();
-  }, [query]);
+  }, [query, filterSelected]);
 
   return (
     <>
       <Header title="Buscar" className="d-lg-none" />
       <div className="container-xxl pt-lg-3">
-        <SearchBar defaultInputValue={query} onSearch={onSearch} onFilterChange={onFilterChange} onInputChange={onInputChange} />
+        <SearchBar defaultInputValue={query} onSearch={onSearch} initialFilter={filterSelected} onFilterChange={onFilterChange} onInputChange={onInputChange} />
       </div>
       {!recent || recent.length == 0 ? (
         ""
@@ -100,7 +136,7 @@ export const Search = () => {
               <ItemCard title={result.title} imgURL={result.imgURL} type={"Lista"} to={`/mis-listas/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
             ))}
             {Object.values(searchResults.items).map((result, i) => (
-              <ItemCard title={result.title} imgURL={result.imgURL} type={"Item"} to={`/mis-listas/_--id${result.listId}/${toUrlSlug(result.title)}--id${result.id}`} key={i} />
+              <ItemCard title={result.title} imgURL={result.imgURL} type={"Item"} to={`/mis-listas/_--id${result.listId}/${toUrlSlug(result.title)}--id${result.id}`} status={result.status} key={i} />
             ))}
           </>
         </section>
