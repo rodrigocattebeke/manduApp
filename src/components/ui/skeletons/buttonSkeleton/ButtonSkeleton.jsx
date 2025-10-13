30a289dbfc0b921d7d15585b5b825fd5e2991876
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-13 15:59:32 -0300

    feat(ButtonSkeleton): add width prop

--- src/components/ui/skeletons/buttonSkeleton/ButtonSkeleton.jsx
+++ src/components/ui/skeletons/buttonSkeleton/ButtonSkeleton.jsx
@@ -1,5 +1,5 @@
 import styles from "./ButtonSkeleton.module.css";
 
-export const ButtonSkeleton = ({ fullWidth = false }) => {
-  return <div className={`${styles.button} ${fullWidth ? styles.fullWidth : ""} skeletonPulse`}></div>;
+export const ButtonSkeleton = ({ fullWidth = false, width = "" }) => {
+  return <div className={`${styles.button} ${fullWidth ? styles.fullWidth : ""} skeletonPulse`} style={{ width }}></div>;
 };
