9159b23ca18b4cb56d6cdf5351e3db28f82e0629
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-01 16:32:09 -0300

    feat(removeListService): delete related items when removing a list

--- src/services/firestore/lists/removeListService.js
+++ src/services/firestore/lists/removeListService.js
@@ -1,13 +1,29 @@
-import { db } from "@/lib/firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "@/lib/firebase";
+import { collection, deleteDoc, doc, getDocs, query, where, writeBatch } from "firebase/firestore";
 
 export const removeListService = async (listId) => {
+  const batch = writeBatch(db);
+
   const listRef = doc(db, "lists", listId);
 
+  const itemsRef = collection(db, "items");
+  const q = query(itemsRef, where("userUID", "==", auth.currentUser.uid), where("listId", "==", listId));
+
   try {
-    await deleteDoc(listRef);
+    //Get all items of the list, and delete
+    const itemsSnapshot = await getDocs(q);
+    itemsSnapshot.forEach((itemDoc) => {
+      batch.delete(itemDoc.ref);
+    });
+
+    //Delete list
+    batch.delete(listRef);
+
+    //Send batch
+    await batch.commit();
     return { success: true };
   } catch (error) {
+    console.error("Error al eliminar lista e ítems:", error.message);
     return { success: false, error };
   }
 };
