c1db747446f494d8e46df78d03a3ae31638397f8
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-03 18:18:58 -0300

    feat(ClientLayout): add userData validation to show navbar

--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -2,18 +2,21 @@
 
 import { Navbar } from "@/components/layout/navbar/Navbar";
 import { HIDDEN_NAVBAR_ROUTES } from "@/constants/hiddenNavbarRoutes";
+import { UserContext } from "@/contexts/UserContext";
 import { usePathname } from "next/navigation";
 import Script from "next/script";
+import { useContext } from "react";
 
 export const ClientLayout = ({ children }) => {
   const pathname = usePathname();
+  const { userData } = useContext(UserContext);
 
   return (
     <>
       {/* layout */}
       <div className="container-xxl p-0 d-flex">
         {/* Hidde navbar in specific routes */}
-        {!HIDDEN_NAVBAR_ROUTES.includes(pathname) && <Navbar />}
+        {!HIDDEN_NAVBAR_ROUTES.includes(pathname) && userData && <Navbar />}
         <main className="container-xxl px-0">{children}</main>
       </div>
       <Script src="/js/bootstrap.bundle.min.js" />
